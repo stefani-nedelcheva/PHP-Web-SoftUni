29e9bac9d5201f219d4a81d72da92513a8d5a8a1
Stefani Nedelcheva <49128892+stefani-nedelcheva@users.noreply.github.com>
2019-05-24 12:23:15 +0300

    updated

--- PHP-Advanced-Syntax/ReverseAnArrayOfStrings.php
+++ PHP-Advanced-Syntax/ReverseAnArrayOfStrings.php
@@ -5,9 +5,3 @@ $numbers = explode(" ", readline());
 for ($a = 0; $a < count($numbers); $a++) {
     echo $numbers[count($numbers) - 1 - $a] . " ";
 }
-
-//for ($i = count($numbers) - 1; $i >= 0; $i--) {
-//    echo $numbers[$i] . " ";
-//}
-
-//echo implode(" ", array_reverse(explode(" ", readline())));
